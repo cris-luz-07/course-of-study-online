d5b1b3a6534a5030bd7e67b600f7f7ae7a8c52b6
Cristina Luz <crisgolfinho@gmail.com>
2019-06-06 00:07:49 -0300

    add  Procfile

--- app.js
+++ app.js
@@ -2,4 +2,6 @@
 let app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
