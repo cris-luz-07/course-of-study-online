f3dab307ca87d91c865e381e16d5c976bc93adb6
cristina luz <crisgolfinho@gmail.com>
2019-06-06 08:19:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /* importar as configurações do servidor */
 let app = require('./config/server');
 
+app.get('/', function(req, res) {
+    res.send('index');
+});
+
 /* parametrizar a porta de escuta */
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
